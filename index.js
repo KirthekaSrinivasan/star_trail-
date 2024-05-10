3eafc5543432f26049c7887948e6ebda966994fe
kirtheka Srinivasan <131374317+kirtehka@users.noreply.github.com>
2024-05-10 17:09:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const bodyE1 = document.querySelector("body")
+bodyE1.addEventListener("mousemove", (event)=>{
+    const xpos = event.offsetX
+    const ypos = event.offsetY
+    const spanE1 = document.createElement("span");
+    spanE1.style.left = xpos + "px";
+    spanE1.style.top = ypos + "px";
+    bodyE1.appendChild(spanE1);
+    setTimeout(() =>{
+        spanE1.remove();
+    }, 3000);
+});
